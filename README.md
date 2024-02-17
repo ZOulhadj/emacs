7fbce5569079157a3ef7e27c9f618db2b3d6f358
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2024-02-17 01:16:30 +0000

    Moved comments from configuration to README.md

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Emacs
+This is my personal programming-focused Emacs configuration. Feel free to copy
+anything to want from my configuration.
 
-This is my personal Emacs configuration.
+## Prerequisites
+- rg (Ripgrep search)
+- aspell (Spelling)
+- aspell-en (English spelling dictionary)
+- mbsync (Mail fetching and sending)
+- mu4e (Mail indexing)
+- mpv (Music Player)
+
+
+## Building
+If building Emacs from source, below are dependencies which are required:
+- tree-sitter (Tree sitter)
+- librsvg2-dev (SVG support)
+- libgccjit0 (Just-in-time compilation)
+
+Compiling from source command:
+``./configure --with-native-compilation --with-json --with-pgtk --with-tree-sitter --with-rsvg``
+
--- init.el
+++ init.el
@@ -2,27 +2,7 @@
 ;;; Commentary:
 ;;; This is my personal programming-focused Emacs configuration file.
 ;;;
-;;; If building Emacs from source, below are dependencies which are required:
-;;; - tree-sitter (Tree sitter)
-;;; - librsvg2-dev (SVG support)
-;;; - libgccjit0 (Just-in-time compilation)
-;;;
-;;; Compiling from source command:
-;;; ./configure --with-native-compilation --with-json --with-pgtk --with-tree-sitter --with-rsvg
-;;;
-;;; Below is a list of applications that this configuration relies on:
-;;; - rg (Ripgrep search)
-;;; - aspell (Spelling)
-;;; - aspell-en (English spelling dictionary)
-;;; - mbsync (Mail fetching and sending)
-;;; - mu4e (Mail indexing)
-;;; - mpv (Music Player)
-;;;
-;;; Below are some commands related to emailing:
-;;; mbsync -c ~/.config/mu4e/mbsyncrc -a
-;;; mu init --maildir=~/Mail/personal --my-address=zakariyaoulhadj01@gmail.com
-;;; mu init --maildir=~/Mail/website --my-address=contact@zakariyaoulhadj.com
-;;; mu index
+
 
 ;;; Code:
 
