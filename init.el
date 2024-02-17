d807506be92ebd9135d56e8569fbd54aca6095d1
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2024-02-17 01:11:23 +0000

    Added a small comment.

--- init.el
+++ init.el
@@ -246,7 +246,7 @@
   (setq recentf-max-saved-items 500
         recentf-max-menu-items 15
         ;; disable recentf-cleanup on Emacs start, because it can cause
-        ;; problems with remote files
+        ;; problems with remote files (prelude)
         recentf-auto-cleanup 'never)
 
   :config
